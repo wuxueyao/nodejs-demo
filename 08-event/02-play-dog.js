2aff9c283332a43efd8f39017307f9da3bae7d11
wuxueyao <2693088968@qq.com>
2019-09-18 14:17:33 +0800

    08

---
+++ 08-event/02-play-dog.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Dog =  require('./02-dog');
+var taidi = new Dog('taidi',4);
+var zangao = new Dog('zangao',9);
+
+taidi.on('bark',function(){
+  console.log('%s barked! energy:%s',this.getName(),this.getEnergy());
+});
+
+
+zangao.on('bark',function(){
+  console.log('%s barked! energy:%s',this.getName(),this.getEnergy());
+});
+
+//可以直接zangao.on('bark',onBark);
+
+function onBark(){
+  console.log('%s barked! energy:%s',this.getName(),this.getEnergy());
+}
---
+++ 08-event/05-radio.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+function Radio(station){
+  var _listeners = {
+
+  };
+
+  EventEmitter.call(this);
+
+  var self = this;
+
+  setTimeout(()=>{
+    self.emit('play',station);
+  },0);
+
+  setTimeout(()=>{
+    self.emit('stop',station)
+  },5000);
+}
+
+function emit(evt,arg){
+  if(typeof(_listeners[evt])==='undefined'){
+    console.error('Error:%s not defined',evt);
+    process.exit(1);
+  }
+
+  _listeners[evt].forEach(fn)=>{
+    fn.call(this,arg);
+  };
+}
+
+this.on = (evt,fn)=>{
+  if(typeof _listeners[evt]==='undefined'){
+    _listeners[evt] = [];
+  }
+  _listeners[evt].push[];
+}
+module.exports = Radio;
