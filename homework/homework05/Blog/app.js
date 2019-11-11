6b6df3c4ddebbb959376e80bf6f0ab414257aa2b
wuxueyao <2693088968@qq.com>
2019-11-11 20:11:37 +0800

    Blog

--- homework/homework05/Blog/app.js
+++ homework/homework05/Blog/app.js
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'views')))
+app.use(express.static(path.join(__dirname,'public/images')))
 
 app.use('/', indexRouter);
 
--- homework/homework05/Blog/routes/index.js
+++ homework/homework05/Blog/routes/index.js
@@ -1,6 +1,7 @@
 const data = require('../public/data/data.json');
 var express = require('express');
 var router = express.Router();
+var isLogin = false;
 
 /* GET home page. */
 
@@ -16,8 +17,10 @@ var list = data.chapterList;
 console.log(data.chapterList[0].title);
 
 router.post('/',function(req,res){
+  isLogin = false;
   console.log(req.body.username)
    if(req.body.username === data.users[0].username && req.body.password === data.users[0].password){
+    isLogin = true;
     res.redirect('/list');
   }else{
     res.redirect('/index');
@@ -25,12 +28,18 @@ router.post('/',function(req,res){
 });
 
 router.get('/list',function(req,res){
+  if(isLogin){
     res.render('list',{titlea:list[0].title,viewsa:list[0].views,
-    titleb:list[1].title,viewsb:list[1].views,
-    titlec:list[2].title,viewsc:list[2].views});
-
-  router.get('/index',function(req,res){
-  res.render('index',{title:'用户名或密码输入错误,请返回重新输入！'});
-  });
+      titleb:list[1].title,viewsb:list[1].views,
+      titlec:list[2].title,viewsc:list[2].views});
+  }else{
+    res.render('index',{title:'请先登录！'})
+  }
 });
+
+router.get('/index',function(req,res){
+  res.render('index',{title:'用户名或密码输入错误,请返回重新输入！'});
+});
+
+
 module.exports = router;
